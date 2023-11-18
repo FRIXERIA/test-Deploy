7dd7d32d964667a5221299748de9eee8190b49ea
Pattharaphon Saefong <89192840+FRIXERIA@users.noreply.github.com>
2023-11-18 14:57:54 +0700

    Delete sas-tt3 directory

--- sas-tt3/src/main/java/sit/int221/sastt3/entities/SubscriptionId.java
+++
@@ -1,9 +0,0 @@
-package sit.int221.sastt3.entities;
-
-import java.io.Serializable;
-
-public class SubscriptionId implements Serializable {
-    private Integer category;
-    private String subscriberEmail;
-
-}
